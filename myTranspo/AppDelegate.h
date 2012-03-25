d5b0e5d36685562f8bec2c5d63a8c78394f8f70a
Vincent Mancini <uhohhotdog@chickenMac.local>
2012-03-25 19:20:03 -0400

    Initial Commit

---
+++ myTranspo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  myTranspo
+//
+//  Created by Vincent Mancini on 12-03-25.
+//  Copyright (c) 2012 Vice Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ myTranspo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myTranspo
+//
+//  Created by Vincent Mancini on 12-03-25.
+//  Copyright (c) 2012 Vice Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
